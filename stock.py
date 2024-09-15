ba80ff479e633b6d05eff0bfc26d9ea8effaa03d
Nina0119 <splendid@hotmail.com.tw>
2024-09-15 15:56:53 +0800

    0915

--- stock.py
+++ stock.py
@@ -68,12 +68,14 @@ def stock_news(stock_name ="大盤"):
   return data
 
 
-def stock_price2(stock_code, history_date=None):
+def stock_price2(stock_code):
     # 指定CSV檔案的路徑
     file_path = '2330.TW_summary.csv'
 
     # 使用pandas讀取CSV檔案
     df = pd.read_csv(file_path)
+
+    history_date=dt.datetime.today()
 
     # 確保日期格式正確
     df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
@@ -88,14 +90,17 @@ def stock_price2(stock_code, history_date=None):
     except ValueError:
         return TextMessage(text="無效的日期格式，請使用 YYYY-MM-DD 格式。")
 
-    # 找到歷史日期的資料
-    historical_data = df[df['Date'] == history_date]
+    # 找到歷史日期前一個月的日期
+    previous_month_date = history_date - pd.DateOffset(months=1)
 
     # Print for debugging
-    print("Data for provided historical date:", historical_data)
+    print("Previous month date:", previous_month_date)
+
+    # 找到前一個月的資料
+    previous_month_data = df[df['Date'] == previous_month_date]
 
     # 根據股票代碼進行進一步篩選
-    matching_row = historical_data[historical_data['公司 代號_x'] == stock_code]
+    matching_row = previous_month_data[previous_month_data['公司 代號_x'] == stock_code]
 
     # Print for debugging
     print("Matching row:", matching_row)
@@ -111,8 +116,7 @@ def stock_price2(stock_code, history_date=None):
         send_text = f"月營收：{month_revenue}\n月營業額費用：{monthly_expense}\n每股盈餘：{eps}"
         message = TextMessage(text=send_text)
     else:
-        send_text = "未找到相對應日期和股票代碼的資料。"
+        send_text = "未找到前一個月對應日期和股票代碼的資料。"
         message = TextMessage(text=send_text)
 
     return message
-
